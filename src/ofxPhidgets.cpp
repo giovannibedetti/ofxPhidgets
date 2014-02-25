95a2ef89f75b4f84cb562a0de7e8c827fbe58c1c
naoto <micuat@aim.com>
2014-02-25 15:26:38 -0500

    Servo working

--- src/ofxPhidgets.cpp
+++ src/ofxPhidgets.cpp
@@ -8,10 +8,138 @@
 // This work is licensed under the Creative Commons Attribution 2.5 Canada License.
 // view a copy of this license, visit http://creativecommons.org/licenses/by/2.5/ca/
 
+// ofxaddon extension by Naoto HIEDA, 2014
+
+#include "ofxPhidgets.h"
+
+// - Stepper simple -
+// This simple example sets up a Stepper object, hooks the event handlers and opens it for device connections.
+// Once an Advanced Servo is attached it will move the motor to various positions.
+//
+// Please note that this example was designed to work with only one Phidget Stepper connected.
+//
+// Copyright 2008 Phidgets Inc.  All rights reserved.
+// This work is licensed under the Creative Commons Attribution 2.5 Canada License.
+// view a copy of this license, visit http://creativecommons.org/licenses/by/2.5/ca/
+
 #include <stdio.h>
 #include <phidget21.h>
+//#include <unistd.h>
 
-int CCONV AttachHandler(CPhidgetHandle IFK, void *userptr)
+int CCONV ofxPhidgetsStepper::PositionChangeHandler(CPhidgetStepperHandle stepper, void *usrptr, int Index, __int64 Value)
+{
+	printf("Motor: %d > Current Position: %lld\n", Index, Value);
+	return 0;
+}
+
+//Display the properties of the attached phidget to the screen.  We will be displaying the name, serial number and version of the attached device.
+int ofxPhidgetsStepper::display_properties(CPhidgetStepperHandle phid)
+{
+	int serialNo, version, numMotors;
+	const char* ptr;
+	
+	CPhidget_getDeviceType((CPhidgetHandle)phid, &ptr);
+	CPhidget_getSerialNumber((CPhidgetHandle)phid, &serialNo);
+	CPhidget_getDeviceVersion((CPhidgetHandle)phid, &version);
+	
+	CPhidgetStepper_getMotorCount (phid, &numMotors);
+	
+	printf("%s\n", ptr);
+	printf("Serial Number: %10d\nVersion: %8d\n# Motors: %d\n", serialNo, version, numMotors);
+	
+	return 0;
+}
+
+void ofxPhidgetsStepper::init()
+{
+	target_pos = 0;
+	
+	//create the stepper object
+	CPhidgetStepper_create(&stepper);
+	
+	//Set the handlers to be run when the device is plugged in or opened from software, unplugged or closed from software, or generates an error.
+	CPhidget_set_OnAttach_Handler((CPhidgetHandle)stepper, ofxPhidgets::AttachHandler, NULL);
+	CPhidget_set_OnDetach_Handler((CPhidgetHandle)stepper, ofxPhidgets::DetachHandler, NULL);
+	CPhidget_set_OnError_Handler((CPhidgetHandle)stepper, ofxPhidgets::ErrorHandler, NULL);
+	
+	//Registers a callback that will run when the motor position is changed.
+	//Requires the handle for the Phidget, the function that will be called, and an arbitrary pointer that will be supplied to the callback function (may be NULL).
+	CPhidgetStepper_set_OnPositionChange_Handler(stepper, ofxPhidgetsStepper::PositionChangeHandler, NULL);
+	
+	//open the device for connections
+	CPhidget_open((CPhidgetHandle)stepper, -1);
+	
+	//get the program to wait for an stepper device to be attached
+	printf("Waiting for Phidget to be attached....");
+	if((result = CPhidget_waitForAttachment((CPhidgetHandle)stepper, 10000)))
+	{
+		CPhidget_getErrorDescription(result, &err);
+		printf("Problem waiting for attachment: %s\n", err);
+		return 0;
+	}
+	
+	//Display the properties of the attached device
+	display_properties(stepper);
+	
+	//read event data
+	printf("Reading.....\n");
+	
+	//This example assumes stepper motor is attached to index 0
+	
+	//Set up some initial acceleration and velocity values
+	CPhidgetStepper_getAccelerationMin(stepper, 0, &minAccel);
+	CPhidgetStepper_setAcceleration(stepper, 0, minAccel*2);
+	CPhidgetStepper_getVelocityMax(stepper, 0, &maxVel);
+	CPhidgetStepper_setVelocityLimit(stepper, 0, maxVel/2);
+	
+	//display current motor position if available
+	if(CPhidgetStepper_getCurrentPosition(stepper, 0, &curr_pos) == EPHIDGET_OK)
+	printf("Motor: 0 > Current Position: %lld\n", curr_pos);
+	
+	//Step 1: Position 0 - also engage stepper
+	printf("Set to position 0 and engage.\n");
+	
+	CPhidgetStepper_setCurrentPosition(stepper, 0, 0);
+	CPhidgetStepper_setEngaged(stepper, 0, 1);
+}
+
+void ofxPhidgetsStepper::update()
+{
+	CPhidgetStepper_getCurrentPosition(stepper, 0, &curr_pos);
+	if( curr_pos != target_pos ) {
+		CPhidgetStepper_setTargetPosition (stepper, 0, target_pos);
+	}
+}
+
+void ofxPhidgetsStepper::setPosition(int _target_pos)
+{
+	target_pos = _target_pos;
+}
+
+int ofxPhidgetsStepper::getPosition() const {
+	return target_pos;
+};
+
+void ofxPhidgetsStepper::exit()
+{
+	CPhidgetStepper_setTargetPosition(stepper, 0, 0);
+	
+	stopped = PFALSE;
+	while(!stopped)
+	{
+		CPhidgetStepper_getStopped(stepper, 0, &stopped);
+		//usleep(100000);
+	}
+	
+	CPhidgetStepper_setEngaged(stepper, 0, 0);
+	
+	//since user input has been read, this is a signal to terminate the program so we will close the phidget and delete the object we created
+	printf("Closing...\n");
+	CPhidget_close((CPhidgetHandle)stepper);
+	CPhidget_delete((CPhidgetHandle)stepper);
+}
+
+int CCONV ofxPhidgets::AttachHandler(CPhidgetHandle IFK, void *userptr)
 {
 	int serialNo;
 	const char *name;
@@ -24,7 +152,7 @@ int CCONV AttachHandler(CPhidgetHandle IFK, void *userptr)
 	return 0;
 }
 
-int CCONV DetachHandler(CPhidgetHandle IFK, void *userptr)
+int CCONV ofxPhidgets::DetachHandler(CPhidgetHandle IFK, void *userptr)
 {
 	int serialNo;
 	const char *name;
@@ -37,7 +165,7 @@ int CCONV DetachHandler(CPhidgetHandle IFK, void *userptr)
 	return 0;
 }
 
-int CCONV ErrorHandler(CPhidgetHandle IFK, void *userptr, int ErrorCode, const char *unknown)
+int CCONV ofxPhidgets::ErrorHandler(CPhidgetHandle IFK, void *userptr, int ErrorCode, const char *unknown)
 {
 	printf("Error handled. %d - %s", ErrorCode, unknown);
 	return 0;
@@ -45,7 +173,7 @@ int CCONV ErrorHandler(CPhidgetHandle IFK, void *userptr, int ErrorCode, const c
 
 //callback that will run if an input changes.
 //Index - Index of the input that generated the event, State - boolean (0 or 1) representing the input state (on or off)
-int CCONV InputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int State)
+int CCONV ofxPhidgets::InputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int State)
 {
 	printf("Digital Input: %d > State: %d\n", Index, State);
 	return 0;
@@ -53,7 +181,7 @@ int CCONV InputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int I
 
 //callback that will run if an output changes.
 //Index - Index of the output that generated the event, State - boolean (0 or 1) representing the output state (on or off)
-int CCONV OutputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int State)
+int CCONV ofxPhidgets::OutputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int State)
 {
 	printf("Digital Output: %d > State: %d\n", Index, State);
 	return 0;
@@ -61,7 +189,7 @@ int CCONV OutputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int 
 
 //callback that will run if the sensor value changes by more than the OnSensorChange trigger.
 //Index - Index of the sensor that generated the event, Value - the sensor read value
-int CCONV SensorChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int Value)
+int CCONV ofxPhidgets::SensorChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int Value)
 {
 	printf("Sensor: %d > Value: %d\n", Index, Value);
 	return 0;
@@ -70,7 +198,7 @@ int CCONV SensorChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int 
 //Display the properties of the attached phidget to the screen.  We will be displaying the name, serial number and version of the attached device.
 //Will also display the number of inputs, outputs, and analog inputs on the interface kit as well as the state of the ratiometric flag
 //and the current analog sensor sensitivity.
-int display_properties(CPhidgetInterfaceKitHandle phid)
+int ofxPhidgetsIfkit::display_properties(CPhidgetInterfaceKitHandle phid)
 {
 	int serialNo, version, numInputs, numOutputs, numSensors, triggerVal, ratiometric, i;
 	const char* ptr;
@@ -101,14 +229,8 @@ int display_properties(CPhidgetInterfaceKitHandle phid)
 }
 
 
-int interfacekit_simple()
+void ofxPhidgetsIfkit::init()
 {
-	int result, numSensors, i;
-	const char *err;
-	
-	//Declare an InterfaceKit handle
-	CPhidgetInterfaceKitHandle ifKit = 0;
-	
 	//create the InterfaceKit object
 	CPhidgetInterfaceKit_create(&ifKit);
 	
@@ -146,11 +268,10 @@ int interfacekit_simple()
 	
 	//read interface kit event data
 	printf("Reading.....\n");
-	
-	//keep displaying interface kit data until user input is read
-	printf("Press any key to go to next step\n");
-	getchar();
-	
+}
+
+void ofxPhidgetsIfkit::update()
+{
 	printf("Modifying sensor sensitivity triggers....\n");
 	
 	//get the number of sensors available
@@ -180,18 +301,12 @@ int interfacekit_simple()
 	printf("Press any key to end\n");
 	getchar();
 	
+}
+
+void ofxPhidgetsIfkit::exit()
+{
 	//since user input has been read, this is a signal to terminate the program so we will close the phidget and delete the object we created
 	printf("Closing...\n");
 	CPhidget_close((CPhidgetHandle)ifKit);
 	CPhidget_delete((CPhidgetHandle)ifKit);
-	
-	//all done, exit
-	return 0;
 }
-
-//int main(int argc, char* argv[])
-//{
-//	interfacekit_simple();
-//	return 0;
-//}
-
--- src/ofxPhidgets.h
+++ src/ofxPhidgets.h
@@ -1,1 +1,71 @@
-int interfacekit_simple();
+// - InterfaceKit simple -
+// This simple example simply creates an InterfaceKit handle, hooks the event handlers and opens it.  It then waits
+// for an InterfaceKit to be attached and waits for events to be fired. We progress through three steps, 1. Normal settings,
+// 2. Setting analog sensor sensitivity to 100, 3. Toggling Ratiometric, waiting for user input to proceed to next step to allow
+// data to be read.
+//
+// Copyright 2008 Phidgets Inc.  All rights reserved.
+// This work is licensed under the Creative Commons Attribution 2.5 Canada License.
+// view a copy of this license, visit http://creativecommons.org/licenses/by/2.5/ca/
+
+// ofxaddon extension by Naoto HIEDA, 2014
+
+#include <stdio.h>
+#include <phidget21.h>
+
+class ofxPhidgets {
+protected:
+	static int CCONV AttachHandler(CPhidgetHandle IFK, void *userptr);
+	static int CCONV DetachHandler(CPhidgetHandle IFK, void *userptr);
+	static int CCONV ErrorHandler(CPhidgetHandle IFK, void *userptr, int ErrorCode, const char *unknown);
+	static int CCONV InputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int State);
+	static int CCONV OutputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int State);
+	static int CCONV SensorChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int Value);
+	
+public:
+	virtual void init() = 0;
+	virtual void update() = 0;
+	virtual void exit() = 0;
+};
+
+class ofxPhidgetsStepper : public ofxPhidgets {
+	
+public:
+	void init();
+	void update();
+	void exit();
+	~ofxPhidgetsStepper() { exit(); };
+	void setPosition(int);
+	int getPosition() const;
+	
+private:
+	static int CCONV PositionChangeHandler(CPhidgetStepperHandle stepper, void *usrptr, int Index, __int64 Value);
+	int display_properties(CPhidgetStepperHandle phid);
+	
+	int result;
+	__int64 curr_pos, target_pos;
+	const char *err;
+	double minAccel, maxVel;
+	int stopped;
+	
+	//Declare an stepper handle
+	CPhidgetStepperHandle stepper = 0;
+};
+
+class ofxPhidgetsIfkit : public ofxPhidgets {
+	
+public:
+	void init();
+	void update();
+	void exit();
+	~ofxPhidgetsIfkit() { exit(); };
+
+private:
+	int display_properties(CPhidgetInterfaceKitHandle phid);
+	
+	int result, numSensors, i;
+	const char *err;
+	
+	//Declare an InterfaceKit handle
+	CPhidgetInterfaceKitHandle ifKit = 0;
+};
